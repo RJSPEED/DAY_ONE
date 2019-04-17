e2679dff70c5eeabe3ad34d9eded3de7f421ea28
RJSPEED <45695434+RJSPEED@users.noreply.github.com>
2019-04-17 14:20:27 +0100

    Add files via upload

--- ccy.py
+++ ccy.py
@@ -1,6 +1,7 @@
-"""Logic = Divide float by largest list element, if amount divides remains move to next
-   element and iterate using remaining amount ... if amount not divisibe move
-   to next number in list.""" 
+"""Logic = Loop through sorted list elements to determine if input_amount
+   divides by element, if it does calc how many times and return, along
+   with element value. Also deduct the related element amount from the
+   input_amount and feed the remainder into the next iteration of the loop.""" 
 def currency_converter(input_amount):
 
     ccy_list = [100,50,10,5,1,0.25,0.10,0.05,0.01]
@@ -10,9 +11,9 @@ def currency_converter(input_amount):
     denom_count = 0 
     amount = input_amount
 
-    """Determine the highest denom."""
     for i in range(len(ccy_list)):
-        """If amount is divisible by element."""
+        """First iteration does input_amount divide by first element value,
+           subsequent iterations use potetially reducing total.""" 
         if amount / ccy_list[i] > 0:
             list_start_pos = i
             denom = ccy_list[i]
@@ -20,7 +21,6 @@ def currency_converter(input_amount):
             remainder = amount - ((amount // ccy_list[i])*ccy_list[i])
             if int(denom_count) > 0:
                 print(int(denom_count)," ", denom)
-            #print(remainder)
         amount = remainder
     
   
